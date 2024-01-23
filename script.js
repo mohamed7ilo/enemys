afa08febbbbbe0fc066fa0ff58d573eb1ca2cc49
mohamed7ilo <149320668+mohamed7ilo@users.noreply.github.com>
2024-01-23 12:33:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numbersOfEnemys = 10;
+const numbersOfEnemys = 20;
 const enemysArr = [];
 function enemy1() {
 
@@ -54,7 +54,7 @@ function enemy1() {
     }
     animate();
 }
-//enemy1();
+enemy1();
 
 
 function enemy2() {
